e8cc17e0fa8cb0b668b633e552ad5c504b99f640
Gasparini Osvaldo <gaspariniosvaldo1@gmail.com>
2021-10-12 21:11:35 -0300

    NavBar

---
+++ src/components/header/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+    return (
+        <header>
+            <h1>Tienda Ecommerce</h1>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Headphones</a></li>
+                <li><a href="#">Speakers</a></li>
+                <li><a href="#">Earphones</a></li>
+            </ul>
+
+        </header>
+    )
+}
